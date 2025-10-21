df693e85effd11ff9f2a58c03f4c40687f965c35
server-imp <66805612+server-imp@users.noreply.github.com>
2025-10-21 17:50:41 +0200

    Refactor versioning to use constexpr and macros

--- src/unpc.hpp
+++ src/unpc.hpp
@@ -9,17 +9,35 @@
 
 namespace unpc
 {
-    extern int32_t signature;
+    constexpr int32_t signature = 1817724315;
 
     namespace version
     {
-        extern int16_t year;
-        extern int16_t month;
-        extern int16_t day;
-        extern int16_t build;
+#define YEAR  2025
+#define MONTH 10
+#define DAY   21
+#define BUILD 2
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+#define VERSION_STRING STR(YEAR) "." STR(MONTH) "." STR(DAY) "." STR(BUILD)
+        constexpr int16_t year  = YEAR;
+        constexpr int16_t month = MONTH;
+        constexpr int16_t day   = DAY;
+        constexpr int16_t build = BUILD;
+
+        constexpr char string[] = VERSION_STRING;
+#undef VERSION_STRING
+#undef BUILD
+#undef DAY
+#undef MONTH
+#undef YEAR
+#undef STR
+#undef STR_HELPER
     }
 
     extern bool loadedByNexus;
+    extern bool loadedByArcDPS;
     extern bool exit;
     extern bool injected;
 
@@ -33,7 +51,9 @@ namespace unpc
     extern HMODULE     hProxyModule;
 
     void start();
+
     void stop();
+
     void entrypoint();
 }
 
