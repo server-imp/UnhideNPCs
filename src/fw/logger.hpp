8cc6fdec2fc93117fb65f4db62d4fd47864869e3
server-imp <66805612+server-imp@users.noreply.github.com>
2026-02-16 01:31:59 +0100

    Format `logger.hpp` for improved readability and consistency, add thread-safety with `std::mutex`, and enhance code structure with minor adjustments.

--- src/fw/logger.hpp
+++ src/fw/logger.hpp
@@ -18,9 +18,9 @@ namespace logging
 
     struct LogEntry
     {
-        std::string timestamp{};
-        LogLevel    level{};
-        std::string message{};
+        std::string timestamp {};
+        LogLevel    level {};
+        std::string message {};
     };
 
     using LogCallback = std::function<void(const LogEntry& entry)>;
@@ -30,22 +30,23 @@ namespace logging
     private:
         static Logger* _instance;
 
-        std::string           _name{};
-        std::filesystem::path _path{};
-        std::ofstream         _file{};
+        std::string           _name {};
+        std::filesystem::path _path {};
+        std::ofstream         _file {};
         LogLevel              _level = LogLevel::Debug;
 
-        HANDLE _hConsole{};
-        bool   _consoleCreated{};
+        HANDLE _hConsole {};
+        bool   _consoleCreated {};
 
-        std::deque<LogEntry>     _recentEntries{};
-        std::vector<LogCallback> _callbacks{};
+        std::deque<LogEntry>     _recentEntries {};
+        std::vector<LogCallback> _callbacks {};
+
+        std::mutex _mutex {};
 
     public:
         static Logger* instance();
 
-        explicit Logger
-        (
+        explicit Logger(
             const std::string&           name,
             const std::filesystem::path& path,
             LogLevel                     level   = LogLevel::Debug,
@@ -62,7 +63,7 @@ namespace logging
 
         void unregisterCallback(const LogCallback& callback);
 
-        template<typename... Args>
+        template <typename... Args>
         void log(LogLevel level, const std::string& format, Args&&... args);
 
         bool setConsole(bool value);
@@ -73,7 +74,7 @@ namespace logging
         void printLogEntry(const LogEntry& entry, bool toFile = true);
     };
 
-    template<typename... Args>
+    template <typename... Args>
     void Logger::log(LogLevel level, const std::string& format, Args&&... args)
     {
         if (level < _level)
@@ -83,11 +84,11 @@ namespace logging
         const auto time = std::chrono::system_clock::to_time_t(now);
         const auto ms   = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
 
-        std::tm tm{};
+        std::tm tm {};
         localtime_s(&tm, &time);
 
         const auto  timestamp = fmt::format("[{:02}:{:02}:{:02}:{:03}]", tm.tm_hour, tm.tm_min, tm.tm_sec, ms.count());
-        std::string message{};
+        std::string message {};
 
         try
         {
@@ -99,9 +100,11 @@ namespace logging
             level   = LogLevel::Error;
         }
 
+        std::lock_guard lock(_mutex);
+
         if (_recentEntries.size() >= 250)
             _recentEntries.pop_front();
-        _recentEntries.push_back({timestamp, level, message});
+        _recentEntries.push_back({ timestamp, level, message });
 
         const auto& entry = _recentEntries.back();
 
@@ -113,7 +116,6 @@ namespace logging
 #define LOG_DBG(fmt, ...) if (logging::Logger::instance()) logging::Logger::instance()->log(logging::LogLevel::Debug, "[{}:{}\t{}()]\t" fmt, util::getFileName(__FILE__), __LINE__, __func__, ##__VA_ARGS__)
 #define LOG_INFO(fmt, ...) if (logging::Logger::instance()) logging::Logger::instance()->log(logging::LogLevel::Info, " " fmt, ##__VA_ARGS__)
 #define LOG_WARN(fmt, ...) if (logging::Logger::instance()) logging::Logger::instance()->log(logging::LogLevel::Warning, " " fmt, ##__VA_ARGS__)
-//#define LOG_ERR(fmt, ...)  if (logging::Logger::instance()) logging::Logger::instance()->log(logging::LogLevel::Error, " " fmt, ##__VA_ARGS__)
 #define LOG_ERR(fmt, ...) if (logging::Logger::instance()) logging::Logger::instance()->log(logging::LogLevel::Error, "[{}:{}\t{}()]\t" fmt, util::getFileName(__FILE__), __LINE__, __func__, ##__VA_ARGS__)
 
 #endif //UNHIDENPCS_LOGGER_HPP
