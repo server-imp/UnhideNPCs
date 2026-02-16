c462e1b658dc3e592a9fe2a7b4e091106b98fa3a
server-imp <66805612+server-imp@users.noreply.github.com>
2026-02-16 01:33:40 +0100

    Add `#pragma pack` to `MumbleLink` and `MumbleContext`, refine formatting, and switch to `OpenFileMappingA` for consistency

--- src/MumbleLink.hpp
+++ src/MumbleLink.hpp
@@ -26,6 +26,7 @@ enum class MapType : uint32_t
     ArmisticeBastion
 };
 
+#pragma pack(push, 1)
 struct MumbleContext
 {
     unsigned char serverAddress[28]; // contains sockaddr_in or sockaddr_in6
@@ -34,17 +35,19 @@ struct MumbleContext
     uint32_t      shardId;
     uint32_t      instance;
     uint32_t      buildId;
-    uint32_t      uiState;
-    uint16_t      compassWidth;    // pixels
-    uint16_t      compassHeight;   // pixels
-    float         compassRotation; // radians
-    float         playerX;         // continentCoords
-    float         playerY;         // continentCoords
-    float         mapCenterX;      // continentCoords
-    float         mapCenterY;      // continentCoords
-    float         mapScale;
-    uint32_t      processId;
-    uint8_t       mountIndex;
+    // Additional data beyond the <context_len> bytes the game instructs Mumble to use to distinguish between instances.
+    uint32_t uiState;
+    // Bitmask: Bit 1 = IsMapOpen, Bit 2 = IsCompassTopRight, Bit 3 = DoesCompassHaveRotationEnabled, Bit 4 = Game has focus, Bit 5 = Is in Competitive game mode, Bit 6 = Textbox has focus, Bit 7 = Is in Combat
+    uint16_t compassWidth;    // pixels
+    uint16_t compassHeight;   // pixels
+    float    compassRotation; // radians
+    float    playerX;         // continentCoords
+    float    playerY;         // continentCoords
+    float    mapCenterX;      // continentCoords
+    float    mapCenterY;      // continentCoords
+    float    mapScale;
+    uint32_t processId;
+    uint8_t  mountIndex;
 
     [[nodiscard]] bool IsMapOpen() const
     {
@@ -81,20 +84,23 @@ struct MumbleContext
         return uiState & (1 << 6);
     }
 };
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 struct MumbleLink
 {
-    uint32_t      uiVersion;
-    uint32_t      uiTick;
-    float         fAvatarPosition[3];
-    float         fAvatarFront[3];
-    float         fAvatarTop[3];
-    wchar_t       name[256];
-    float         fCameraPosition[3];
-    float         fCameraFront[3];
-    float         fCameraTop[3];
-    wchar_t       identity[256];
-    uint32_t      context_len;
+    uint32_t uiVersion;
+    uint32_t uiTick;
+    float    fAvatarPosition[3];
+    float    fAvatarFront[3];
+    float    fAvatarTop[3];
+    wchar_t  name[256];
+    float    fCameraPosition[3];
+    float    fCameraFront[3];
+    float    fCameraTop[3];
+    wchar_t  identity[256];
+    uint32_t context_len;
+    // Despite the actual context containing more data, this value is currently 48. See "context" section below.
     unsigned char context[256];
     wchar_t       description[2048];
 
@@ -103,19 +109,26 @@ struct MumbleLink
         return *reinterpret_cast<MumbleContext*>(&context);
     }
 };
+#pragma pack(pop)
 
 inline MumbleLink* getMumbleLink()
 {
-    std::wstring mappingName = util::getStartupArgValue(L"-mumble");
+    std::string mappingName = util::getStartupArgValue("-mumble");
     if (mappingName.empty())
-        mappingName = L"MumbleLink";
+        mappingName = "MumbleLink";
 
-    auto hMap = OpenFileMappingW(FILE_MAP_ALL_ACCESS, FALSE, mappingName.c_str());
+    auto hMap = OpenFileMappingA(FILE_MAP_ALL_ACCESS, FALSE, mappingName.c_str());
     if (!hMap)
     {
-        LOG_DBG("OpenFileMappingW failed: {}", GetLastError());
-        hMap = CreateFileMappingW
-            (INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, sizeof(MumbleLink), mappingName.c_str());
+        LOG_DBG("OpenFileMappingW failed: \"{}\", {}", mappingName, GetLastError());
+        hMap = CreateFileMappingA(
+            INVALID_HANDLE_VALUE,
+            nullptr,
+            PAGE_READWRITE,
+            0,
+            sizeof(MumbleLink),
+            mappingName.c_str()
+        );
 
         if (!hMap)
         {
@@ -134,7 +147,9 @@ inline MumbleLink* getMumbleLink()
         return nullptr;
     }
 
-    CloseHandle(hMap);
+    // The game won't initialize Mumble Link if we close this handle
+    // CloseHandle(hMap);
+
     return static_cast<MumbleLink*>(ptr);
 }
 #endif //UNHIDENPCS_MUMBLELINK_HPP
