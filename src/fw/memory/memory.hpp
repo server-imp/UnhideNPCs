bf11c6af1eb65273c1466d2bd17d467c76677bd1
server-imp <66805612+server-imp@users.noreply.github.com>
2026-02-16 01:32:40 +0100

    Add `locate_all_pointers` to `memory.hpp` to locate pointers within specified bounds

--- src/fw/memory/memory.hpp
+++ src/fw/memory/memory.hpp
@@ -13,6 +13,13 @@
 namespace memory
 {
     bool try_near_alloc(const handle& target, size_t size, handle& result);
+
+    bool locate_all_pointers(
+        const handle&        base,
+        size_t               largest_offset,
+        const handle&        target,
+        std::vector<handle>& results
+    );
 }
 
 #endif //UNHIDENPCS_MEMORY_HPP
