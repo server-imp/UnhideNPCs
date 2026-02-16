21b299b82509836c4d394d607c85697fe50efa57
server-imp <66805612+server-imp@users.noreply.github.com>
2026-02-16 01:36:56 +0100

    Refactor `settings.hpp` to improve formatting consistency, adjust property declarations, and add new overlay configuration options.

--- src/settings.hpp
+++ src/settings.hpp
@@ -6,8 +6,7 @@
 class Settings : public Config
 {
 private:
-    CONFIG_PROPERTY
-    (
+    CONFIG_PROPERTY(
         bool,
         ForceConsole,
         false,
@@ -17,21 +16,23 @@ private:
 
     CONFIG_PROPERTY(bool, AlwaysShowTarget, true, "Always show the targeted character, even if it would be hidden.")
 
-    CONFIG_PROPERTY
-    (bool, PlayerOwned, false, "NPCs that are owned by players (pets, clones, minis etc) will also be unhidden.")
+    CONFIG_PROPERTY(
+        bool,
+        PlayerOwned,
+        false,
+        "NPCs that are owned by players (pets, clones, minis etc) will also be unhidden."
+    )
 
     CONFIG_PROPERTY(int32_t, MinimumRank, 0, "Only NPCs that have at least this rank get unhidden.")
 
-    CONFIG_PROPERTY
-    (
+    CONFIG_PROPERTY(
         int32_t,
         Attackable,
         0,
         "Only NPCs that match this get unhidden.\n0: Both\n1: Only Attackable\n2: Only Non-Attackable"
     )
 
-    CONFIG_PROPERTY
-    (
+    CONFIG_PROPERTY(
         bool,
         HidePlayers,
         false,
@@ -39,16 +40,19 @@ private:
         "Their names are still visible, and you can still target them"
     )
 
-    CONFIG_PROPERTY(uint32_t, MaxPlayersVisible, 0, "Maximum number of visible players. (0=no limit)")
-    CONFIG_PROPERTY(uint32_t, MaxPlayerOwnedVisible, 0, "Maximum number of visible player owned NPCs. (0=no limit)")
+    CONFIG_PROPERTY(int32_t, MaxPlayersVisible, 0, "Maximum number of visible players. (0=no limit)")
+    CONFIG_PROPERTY(int32_t, MaxPlayerOwnedVisible, 0, "Maximum number of visible player owned NPCs. (0=no limit)")
 
-    CONFIG_PROPERTY
-    (bool, HidePlayerOwned, false, "NPCs that are owned by players (pets, clones, minis etc) will be hidden.")
+    CONFIG_PROPERTY(
+        bool,
+        HidePlayerOwned,
+        false,
+        "NPCs that are owned by players (pets, clones, minis etc) will be hidden."
+    )
 
     CONFIG_PROPERTY(bool, HidePlayerOwnedSelf, false, "Also hide NPCs that are owned by you.")
 
-    CONFIG_PROPERTY
-    (
+    CONFIG_PROPERTY(
         bool,
         DisableHidingInInstances,
         false,
@@ -57,8 +61,7 @@ private:
 
     CONFIG_PROPERTY(float, MaximumDistance, 0, "NPCs within this distance will be unhidden. (0=no distance check)")
 
-    CONFIG_PROPERTY
-    (
+    CONFIG_PROPERTY(
         bool,
         LoadScreenBoost,
         false,
@@ -68,6 +71,13 @@ private:
     )
 
     CONFIG_PROPERTY(bool, ArcDPS_UIOpen, true, "ArcDPS UI Opened/Closed")
+    CONFIG_PROPERTY(bool, DisableOverlay, false, "Disable the built in overlay when using Injection or Proxy mode")
+    CONFIG_PROPERTY(
+        float,
+        OverlayFontSize,
+        24.0f,
+        "The font size used for the overlay\nRequires a restart/reload to reflect changes"
+    )
 
 public:
     explicit Settings(const std::filesystem::path& filePath)
